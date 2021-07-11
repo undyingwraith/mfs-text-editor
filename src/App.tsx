da572fe0b8797526d4a7393deab0bb0a535d386a
undyingwraith <undyingwraith@outlook.com>
2021-07-12 01:05:08 +0200

    prettier ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import React, {useMemo, useState} from 'react';
 import './App.css';
 import {MfsEditor} from './MfsEditor';
+import {Button, Textarea, Label, Pane, TextInput, toaster} from 'evergreen-ui';
 
 function App() {
+	const [currentPath, setCurrentPath] = useState('')
 	const [path, setPath] = useState('/')
 	const [text, setText] = useState('')
 	const [originalText, setOriginalText] = useState('')
@@ -14,6 +16,7 @@ function App() {
 		editor.readFile(path).then(text => {
 			setText(text);
 			setOriginalText(text);
+			setCurrentPath(path);
 		}).catch(e => {
 			console.error(e)
 			alert(e)
@@ -22,31 +25,42 @@ function App() {
 		})
 	}
 
-	const writeFile = () => {
+	const writeFile = (saveAs = false) => {
 		setIsWorking(true)
-		editor.writeFile(path, text).then(() => {
+		editor.writeFile(saveAs ? path : currentPath, text).then(() => {
 			setOriginalText(text);
-			alert('file saved')
+			toaster.notify('file saved')
 		}).catch(e => {
 			console.error(e)
-			alert(e)
+			toaster.danger(e)
 		}).finally(() => {
 			setIsWorking(false)
 		})
 	}
 
-	return <>
-		<header>
-			<input value={path} onChange={(e) => setPath(e.target.value)} disabled={isWorking} size={250}/>
-			<button onClick={loadFile} disabled={isWorking}>Open</button>
-			<button onClick={writeFile} disabled={isWorking}>Save</button>
-		</header>
-		<div>
-			changed: {(text !== originalText) + ""}
-			{isWorking && '| Working...'}
-		</div>
-		<main><textarea onChange={(e) => setText(e.target.value)} value={text} disabled={isWorking}/></main>
-	</>;
+	return <Pane padding={25}>
+		<Pane>
+			<TextInput onChange={(e: any) => setPath(e.target.value)} value={path} disabled={isWorking}/>
+			<Button marginRight={16}  onClick={loadFile} disabled={isWorking}>Open</Button>
+			<Button onClick={() => writeFile()} disabled={isWorking} marginRight={16} appearance="primary">
+				Save
+			</Button>
+			<Button onClick={() => writeFile(true)} disabled={isWorking} marginRight={16}>
+				Save as
+			</Button>
+		</Pane>
+		<Pane>
+			<Label htmlFor="textarea-2" marginBottom={4} display="block">
+				{currentPath}{originalText !== text && '*'}
+			</Label>
+			<Textarea
+				id="textarea-2"
+				value={text}
+				disabled={isWorking}
+				onChange={(e: any) => setText(e.target.value)}
+			/>
+		</Pane>
+	</Pane>;
 }
 
 export default App;
